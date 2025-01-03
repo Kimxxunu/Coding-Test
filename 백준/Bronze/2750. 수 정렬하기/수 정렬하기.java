c28174b265c69ea25fecdd19d6ffc3a657b3e3b6
SUNNY_KIM <114386406+Kimxxunu@users.noreply.github.com>
2025-01-04 01:41:21 +0900

    [Bronze II] Title: 수 정렬하기, Time: 136 ms, Memory: 16152 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,26 +1,30 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
- 
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-    
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-		
-		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
- 
-		// 정렬 메소드
-		Arrays.sort(arr);
-		
-		for(int val : arr) {
-			System.out.println(val);
-		}
- 
-	}
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        /// 정렬 메소드
+        Arrays.sort(arr);
+
+
+        // 정렬된 배열 출력
+        for (int i = 0; i < n; i++) {
+            bw.write(arr[i] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
