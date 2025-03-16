7378a28ae25ef932776436668019771d3ebcf799
SUNNY_KIM <114386406+Kimxxunu@users.noreply.github.com>
2025-03-16 19:52:25 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 108 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,30 +1,32 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args)  {
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt(),m=sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                sb.append(arr[i][j] + Integer.parseInt(st.nextToken()) + " ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
 
-        int A[][] = new int[n][m];
-        int B[][] = new int[n][m];
-        int C[][] = new int[n][m];
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                A[i][j] = sc.nextInt();
-            }
-        }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                B[i][j] = sc.nextInt();
-            }
-        }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                C[i][j]=A[i][j]+B[i][j];
-                System.out.print(C[i][j]+" ");
-            }
-            System.out.println();
-        }
+
+
     }
+
 }
