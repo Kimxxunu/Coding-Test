7b60d6dfec62b5642b3cf94e9e5d167724fd0e02
SUNNY_KIM <114386406+Kimxxunu@users.noreply.github.com>
2025-05-04 13:23:24 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main(int argc, char const *argv[]) {
+    
+    int A;
+    int B;
+ 
+    scanf("%d", &A);
+    scanf("%d", &B);
+ 
+    // scanf("%d %d", &A, &B); 로 바꾸어도 무방
+    
+    printf("%d", A + B);
+    return 0;
+}
