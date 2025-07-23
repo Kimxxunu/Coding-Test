1da60847270c6628e0bbbc399bda4f8351234c10
SUNNY_KIM <114386406+Kimxxunu@users.noreply.github.com>
2025-07-23 14:40:12 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 39.08 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,23 +1,17 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int []arr) {
+    public Stack<Integer> solution(int []arr) {
 
         
-        Deque<Integer> st = new ArrayDeque<>();   
+        Stack<Integer> st = new Stack<>();   
         
         for(int a : arr){
             if (!st.isEmpty() && st.peek() == a) continue;
             st.push(a);
         }
         
-        int[] answer = new int[st.size()];
-        int i=0;
-        while(!st.isEmpty()){
-            answer[i] = st.pollLast();
-            i++;
-        }
-        
-        return answer;
+       
+        return st;
     }
 }
