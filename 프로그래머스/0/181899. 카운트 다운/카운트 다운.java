f2892480eb84f2f8a2374ffc55cefb322f6e86c5
SUNNY_KIM <114386406+Kimxxunu@users.noreply.github.com>
2025-08-14 03:41:27 +0900

    [level 0] Title: 카운트 다운, Time: 0.04 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public List<Integer> solution(int start_num, int end_num) {
+        List<Integer> li = new ArrayList<>();
+        for(int i=start_num; i>=end_num; i--)li.add(i);
+        return li;
+    }
+}
