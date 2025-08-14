a22046040261981f3bda00be8f896e9b1eb467cf
SUNNY_KIM <114386406+Kimxxunu@users.noreply.github.com>
2025-08-14 20:33:29 +0900

    [level 0] Title: n의 배수, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int num, int n) {
+        int answer = 0;
+        if(num%n == 0) answer =1;
+        else answer = 0;
+        
+        return answer;
+    }
+}
